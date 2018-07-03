183600052c1d7829327a2138f6016d5e3b3e6758
sutran0689 <40629466+sutran0689@users.noreply.github.com>
2018-07-03 10:35:10 +0700

    test oled nang cao

---
+++ test-oled2.c
@@ -0,0 +1,106 @@
+
+#include <Arduino.h>
+#include <U8x8lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
+
+void setup(void)
+{
+  u8x8.begin();
+  //u8x8.setFlipMode(1);
+}
+void pre(void)
+{
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);    
+  u8x8.clear();
+  u8x8.inverse();
+  u8x8.setCursor(4,0);
+  u8x8.print("SuTran");
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f); 
+  u8x8.noInverse();
+  u8x8.setCursor(0,1);
+}
+
+void draw_bar(uint8_t c, uint8_t is_inverse)
+{	
+  uint8_t r;
+  u8x8.setInverseFont(is_inverse);
+  for( r = 0; r < u8x8.getRows(); r++ )
+  {
+    u8x8.setCursor(c, r);
+    u8x8.print(" ");
+  }
+}
+
+void draw_ascii_row(uint8_t r, int start)
+{
+  int a;
+  uint8_t c;
+  for( c = 0; c < u8x8.getCols(); c++ )
+  {
+    u8x8.setCursor(c,r);
+    a = start + c;
+    if ( a <= 255 )
+      u8x8.write(a);   // in ra ma ascii
+  }
+}
+
+void loop(void)
+{
+  int i;
+  uint8_t c, r, d;
+  pre();
+  u8x8.print("Hello");
+  u8x8.setCursor(0,2);
+  u8x8.print("My name is su");
+  u8x8.setCursor(0,3);
+  u8x8.print("I'm 29 years old");
+  u8x8.setCursor(0,4);
+  u8x8.print("I'm from VN");
+  u8x8.setCursor(0,5);
+  u8x8.print("Nice to meet you"); 
+  u8x8.setCursor(0,6); 
+  u8x8.print((int)u8x8.getCols());
+  u8x8.print("x");
+  u8x8.print((int)u8x8.getRows());
+ 
+  delay(2000);
+
+  pre();
+  for( i = 30; i > 0; i-- )
+  {
+    u8x8.setCursor(7,4);
+    u8x8.print(i);
+    u8x8.print("  ");
+    delay(200);
+  }
+
+  draw_bar(0, 1);
+  for( c = 1; c < u8x8.getCols(); c++ )
+  {
+    draw_bar(c, 1);
+    draw_bar(c-1, 0);
+    delay(50);
+  }
+   draw_bar(u8x8.getCols()-1, 0);
+
+  pre();
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f); 
+  for( d = 0; d < 8; d ++ )
+  {
+    for( r = 1; r < u8x8.getRows(); r++ )
+    {
+      draw_ascii_row(r, (r-1+d)*u8x8.getCols() + 32);
+    }
+    delay(800);
+  }
+
+  //while(1);
+}
